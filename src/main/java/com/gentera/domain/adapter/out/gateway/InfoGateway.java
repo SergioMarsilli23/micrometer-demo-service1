75b6f92f61fa46b6ea21abe881fb028e43c841ad
sermartinez <sermartinez@host.docker.internal>
2023-06-09 10:58:14 -0600

    commit

--- src/main/java/com/gentera/domain/adapter/out/gateway/InfoGateway.java
+++ src/main/java/com/gentera/domain/adapter/out/gateway/InfoGateway.java
@@ -4,10 +4,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import com.gentera.domain.adapter.out.exception.CircuitBreakerException;
-
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
-import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 
 import reactor.core.publisher.Mono;
 import lombok.extern.slf4j.Slf4j;
@@ -17,14 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class InfoGateway {
 
-    private final ReactiveCircuitBreaker reactiveCircuitBreaker;
     private final WebClient.Builder webClientBuilder;
 
     private String url = "http://localhost:8090/rest";
 
-    public InfoGateway(ReactiveCircuitBreakerFactory circuitBreakerFactory, 
-    		WebClient.Builder webClientBuilder) {
-        this.reactiveCircuitBreaker = circuitBreakerFactory.create("myCircuitBreaker");
+    public InfoGateway(WebClient.Builder webClientBuilder) {
         this.webClientBuilder = webClientBuilder;
     }
 
@@ -32,14 +25,10 @@ public class InfoGateway {
         WebClient webClient = webClientBuilder
                 .baseUrl(url).build();
         
-        return reactiveCircuitBreaker.run(webClient.get()
+        return webClient.get()
                 .uri("/info")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
-                .bodyToMono(String.class),
-                throwable -> {
-                    return Mono.error(new CircuitBreakerException(
-                            "El servicio no responde en este momento."));
-                });
+                .bodyToMono(String.class);
     }
 }
