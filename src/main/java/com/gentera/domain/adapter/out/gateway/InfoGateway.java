81c5d0415b9af8b8f0ba1d226462719fc5090c4c
sermartinez <sermartinez@host.docker.internal>
2023-06-09 11:55:05 -0600

    circuit breaker

--- src/main/java/com/gentera/domain/adapter/out/gateway/InfoGateway.java
+++ src/main/java/com/gentera/domain/adapter/out/gateway/InfoGateway.java
@@ -5,6 +5,10 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.gentera.domain.adapter.out.exception.CircuitBreakerException;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+
 import reactor.core.publisher.Mono;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,10 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 public class InfoGateway {
 
     private final WebClient.Builder webClientBuilder;
+    private final ReactiveCircuitBreaker reactiveCircuitBreaker;
 
     private String url = "http://localhost:8090/rest";
 
-    public InfoGateway(WebClient.Builder webClientBuilder) {
+    public InfoGateway(ReactiveCircuitBreakerFactory circuitBreakerFactory, 
+    		WebClient.Builder webClientBuilder) {
+        this.reactiveCircuitBreaker = circuitBreakerFactory.create("myCircuitBreaker");
         this.webClientBuilder = webClientBuilder;
     }
 
@@ -25,10 +32,14 @@ public class InfoGateway {
         WebClient webClient = webClientBuilder
                 .baseUrl(url).build();
         
-        return webClient.get()
+        return reactiveCircuitBreaker.run(webClient.get()
                 .uri("/info")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(String.class),
+                throwable -> {
+                    return Mono.error(new CircuitBreakerException(
+                            "El servicio no responde en este momento."));
+                });
     }
 }
